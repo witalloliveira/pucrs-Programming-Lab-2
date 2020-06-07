166cf0dfb0d26d0ac522da537d93d599576dd331
Witallo Oliveira <witallo@outlook.com>
2020-06-07 17:50:58 -0300

    fix(T1): Ajustes na implementação da classe Telefone

--- T1/src/Telefone.cpp
+++ T1/src/Telefone.cpp
@@ -7,38 +7,38 @@ using namespace std;
 Telefone::Telefone()
 {
     setDDI(0);
-    setDDI(0);
-    setDDI(0);
+    setDDD(0);
+    setNumero(0);
 }
 
 Telefone::Telefone(int ddi, int ddd, long numero)
 {
     setDDI(ddi);
-    setDDI(ddd);
-    setDDI(numero);
+    setDDD(ddd);
+    setNumero(numero);
 }
 
 Telefone::Telefone(Telefone &telefone)
 {
     setDDI(telefone.getDDI());
-    setDDI(telefone.getDDD());
-    setDDI(telefone.getNumero());
+    setDDD(telefone.getDDD());
+    setNumero(telefone.getNumero());
 }
 
-int Telefone::getDDI() { return ddi; }
+int Telefone::getDDI() { return this->ddi; }
 void Telefone::setDDI(int ddi) { this->ddi = ddi; }
 
-int Telefone::getDDD() { return ddd; }
+int Telefone::getDDD() { return this->ddd; }
 void Telefone::setDDD(int ddd) { this->ddd = ddd; }
 
-long Telefone::getNumero() { return numero; }
+long Telefone::getNumero() { return this->numero; }
 void Telefone::setNumero(long numero) { this->numero = numero; }
 
 string Telefone::toString()
 {
-    string strDDI = to_string(getDDI());
-    string strDDD = to_string(getDDD());
-    string strNumero = to_string(getNumero());
+    string strDDI = to_string(this->getDDI());
+    string strDDD = to_string(this->getDDD());
+    string strNumero = to_string(this->getNumero());
 
     string subStrNumeroBegin = strNumero.substr(0, 4);
     string subStrNumeroEnd = strNumero.substr(4, 4);
@@ -55,9 +55,9 @@ string Telefone::toString()
 
 string Telefone::toCSV(char separador)
 {
-    string strDDI = to_string(getDDI());
-    string strDDD = to_string(getDDD());
-    string strNumero = to_string(getNumero());
+    string strDDI = to_string(this->getDDI());
+    string strDDD = to_string(this->getDDD());
+    string strNumero = to_string(this->getNumero());
 
     return (strDDI +
             separador +
